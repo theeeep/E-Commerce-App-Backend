38cfba3093ff2a704995746f5f2ad4394651eb3b
Deepak <ideepakrabindra@gmail.com>
2024-06-18 11:56:40 +0530

    Integrate Swagger for Api Docs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Express } from 'express';
 import rootRouter from 'rootRouter';
 import { errorMiddleware } from 'middlewares/errors';
+import postmanToOpenApi from 'postman-to-openapi';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerJson from '../openapi.json';
 
 const app: Express = express();
 
@@ -8,10 +12,32 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+
 // Routes Middleware
 app.use('/api', rootRouter);
 
 // Error Middleware
 app.use(errorMiddleware);
+
+app.get('/generate-yml', async () => {
+  // Postman Collection Path
+  const postmanCollection = 'collection.json';
+
+  // Output OpenAPI Path
+  const outputFile = 'collection.yml';
+
+  // Async/await
+  try {
+    const result = await postmanToOpenApi(postmanCollection, outputFile, {
+      defaultTag: 'General',
+    });
+
+    // Without save the result in a file
+    console.log(`OpenAPI specs: ${result}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default app;
